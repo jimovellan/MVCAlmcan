7fe9ccd12104d3a3973ff79d0a3d8a9a0fcfa28f
nmovellan <jimovellan@gmail.com>
2018-04-29 22:52:49 +0200

    Init

---
+++ MVCAlmacen/MVCAlmacen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAlmacen.MvcApplication" Language="C#" %>
---
+++ MVCAlmacen/MVCAlmacen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAlmacen.Startup))]
+namespace MVCAlmacen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
